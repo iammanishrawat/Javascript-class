fe216877849137558f5f6537bc7656088475687f
Manish rawat <manish.rawat@essencesoftwares.com>
2024-03-03 15:05:34 +0530

    concat method in array added

--- ArrayClass/script.js
+++ ArrayClass/script.js
@@ -1,17 +1,43 @@
-const fruitCollection = [
-  'Apple',
-  'Banana',
-  'Grapes',
-  'Oranges',
-  5,
-  true,
-  false,
-  'Nan',
-  undefined,
-  null,
-  ['Manish', 'Singh', 'Rawat'],
-];
+// array class stars here
 
-fruitCollection[fruitCollection.length] = 'Chiku';
+// const fruitCollection = [
+//   'Apple',
+//   'Banana',
+//   'Grapes',
+//   'Oranges',
+//   5,
+//   true,
+//   false,
+//   'Nan',
+//   undefined,
+//   null,
+//   ['Manish', 'Singh', 'Rawat'],
+// ];
 
-fruitCollection.push('Citrus')
+// fruitCollection[fruitCollection.length] = 'Chiku';
+
+// fruitCollection.push('Citrus')
+
+// array class ends here
+
+// array method class starts here
+
+// const evenNumber = [2, 4, 6, 8, 10];
+
+// evenNumber.shift();
+
+// evenNumber.unshift(0);
+// evenNumber.unshift(-2);
+// evenNumber.unshift(-4);
+
+// console.log(evenNumber);
+
+const animalLists = ['Tiger', 'Lion', 'Horse', 'Wolf', 'Fox'];
+
+const birdName = ['Parrot', 'Crow', 'Dove', 'Eagle', 'Vulture'];
+
+const finalResult = animalLists.concat(birdName);
+
+console.log(finalResult);
+
+// array method class ends here
