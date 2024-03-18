4d9ca22b5d1a707aca2dd64ea95ce6f54c4201bb
Manish rawat <manish.rawat@essencesoftwares.com>
2024-03-18 14:13:40 +0530

    function class added

--- Introduction-of-function/script.js
+++ Introduction-of-function/script.js
@@ -1,21 +1,15 @@
-function introduction() {
-  console.log('Hello,');
-  console.log('My name is Manish Rawat');
-  console.log('i am a Frontend developer');
+function introduceMe(userName, userProfile, userAge) {
+    // if (!userName) {
+    //     userName = 'Dummy'
+    // }
+    console.log('Hello,');
+    console.log(`My name is ${userName || 'Dummy'}`);
+    console.log(`And i am a ${userProfile || 'Working Professional'}`);
+    console.log(`I am ${userAge} years old`);
 
-  console.log('*************************************');
-
-  console.log('Hello,');
-  console.log('My name is Manish Rawat');
-  console.log('i am a Frontend developer');
-
-  console.log('*************************************');
-
-  console.log('Hello,');
-  console.log('My name is Manish Rawat');
-  console.log('i am a Frontend developer');
-
-//   return 'Name';
+    // return 'Name';
 }
 
-const returnValue = introduction();
+// const returnValue = introduceMe();
+introduceMe('Manish Rawat', 'Software Engineer', 29);
+introduceMe('Sujeet Kumar', 'SDE3', 32);
