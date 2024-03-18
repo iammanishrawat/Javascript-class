b3dff24aba5efd6401af0218f188a2cd99e4b9c0
Manish rawat <manish.rawat@essencesoftwares.com>
2024-03-18 15:53:10 +0530

    return class added

---
+++ Return-class/script.js
@@ -0,0 +1,18 @@
+function addTwoNumbers(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+
+const result = addTwoNumbers(100, 14);
+
+console.log(result);
+
+
+// function subtractTwoNumbers(firstNum, secondNum) {
+//     return firstNum - secondNum;
+//   }
+  
+  
+//   const result = subtractTwoNumbers(100, 14);
+  
+//   console.log(result);
